a65701facf3e6ace9e2489644ff41771f9cedd49
Lautaro Fernando Cornejo Arcos <ltrcornejo@gmail.com>
2023-06-07 19:40:25 -0400

    Initial commit

---
+++ app/src/main/java/com/example/app1awake/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.app1awake;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = MainActivity.class.getSimpleName();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView myTitle = findViewById(R.id.miTitulo);
+        myTitle.setText(R.string.miTitulo);
+
+        TextView myName = findViewById(R.id.miNombre);
+        myName.setText(R.string.miNombre);
+
+        TextView mySur = findViewById(R.id.miApellido);
+        mySur.setText(R.string.miApellido);
+
+        TextView myEmail = findViewById(R.id.miMail);
+        myEmail.setText(R.string.miMail);
+
+        Button mySend = findViewById(R.id.miEnvio);
+        mySend.setText(R.string.miEnvio);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"On Start!");
+    }
+    public void botonClick(View view) {
+        Button mySend = findViewById(R.id.miEnvio);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.boton_resize);
+        mySend.startAnimation(animation);
+        Intent intent = new Intent(this, MuestraDatos.class);
+        EditText nomText = findViewById(R.id.editNombre);
+        EditText apeText = findViewById(R.id.editApellido);
+        EditText maiText = findViewById(R.id.editEmail);
+        intent.putExtra("nombre", nomText.getText().toString());
+        intent.putExtra("apellido", apeText.getText().toString());
+        intent.putExtra("email", maiText.getText().toString());
+        startActivity(intent);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"On Resume!");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"On Pausa!");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"On Stop!");
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG,"On Restart!");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"On Destroy!");
+    }
+}
---
+++ app/src/main/java/com/example/app1awake/MuestraDatos.java
@@ -0,0 +1,29 @@
+package com.example.app1awake;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MuestraDatos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_muestra_datos);
+
+        TextView myTitle = findViewById(R.id.miTituloReg);
+        myTitle.setText(R.string.miTitulo2);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            // Obtener los valores utilizando las claves
+            String nombre = extras.getString("nombre");
+            String apellido = extras.getString("apellido");
+            String email = extras.getString("email");
+            // y se muestran
+            TextView myData = findViewById(R.id.muestraDato);
+            myData.setText(nombre + " " +  apellido + "                                   " + email);
+
+        }
+    }
+}
